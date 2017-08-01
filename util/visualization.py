f80ce23bd051e6269b49ffd67e06ae514fc67865
Chia-Hao Hsieh <chhsieh@ebay.com>
2017-08-01 14:11:36 -0700

    added TODO in visualization.py

---
+++ util/visualization.py
@@ -0,0 +1,15 @@
+import matplotlib
+
+def visualize(image, target):
+    '''
+    input:
+      image:  a numpy array of shape (num of channels, Height, Width)
+      target: a numpy array of shape (1, Height, Width) with only 1's and 0's in it
+    '''
+    assert image.shape == target.shape
+
+    # TODO use matplotlib to draw three figures side-by-side:
+    # image, target, image overlaied with target
+    # like so: https://kaggle2.blob.core.windows.net/forum-message-attachments/208218/6916/starter-kit.png
+
+    return
