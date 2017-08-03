ee63eddf534d4cafc607ea259d1ead668825aefc
Chia-Hao Hsieh <chhsieh@ebay.com>
2017-08-03 15:11:13 -0700

    added accuracy for display; show stats for epoch instead of step

--- train.py
+++ train.py
@@ -12,6 +12,7 @@ except ImportError:
 import model.loss.CrossEntropy2dLoss as CrossEntropy2dLoss
 import model.loss.StableBCELoss as StableBCELoss
 import util.exp as exp
+import util.evaluation as evaluation
 from dataloader import *
 import config
 
@@ -41,14 +42,15 @@ experiment, use_tensorboard = exp.setup_crayon(use_tensorboard, CrayonClient, ex
 # Training setting
 DEBUG = cfg['DEBUG']
 log_iter_interval     = cfg['log_iter_interval']
-display_iter_interval = cfg['display_iter_interval']
+# display_iter_interval = cfg['display_iter_interval'] # TODO remove this param from all .yml files
 snapshot_epoch_interval = cfg['snapshot_epoch_interval']
 num_epochs = cfg['num_epochs']
 
 # Train the Model
 for epoch in range(start_epoch, num_epochs + 1):
     epoch_start = time.time()
-    epoch_loss = 0
+    epoch_train_loss = 0
+    epoch_accuracy   = 0
 
     print('Epoch [%d/%d] starts'
           % (epoch, num_epochs))
@@ -72,34 +74,43 @@ for epoch in range(start_epoch, num_epochs + 1):
         outputs = net(images)
         loss = criterion(outputs, targets)
 
+        # generate prediction
+        masks = (outputs > 0.5).float()
+        accuracy  = evaluation.dice(masks, targets) # TODO dice takes only 2-dim inputs
+
         # Backward + Optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        epoch_loss += loss.data[0]
-
-        if use_tensorboard and (i + 1) % display_iter_interval == 0:
-            step = len(data_loader) * (epoch - 1) + (i + 1)
-            loss_in_np = loss.data[0]
-            experiment.add_scalar_value('train_loss', loss_in_np, step=step)
-            # exp.add_scalar_value('learning_rate', lr, step=step)
+        # Update epoch stats
+        epoch_train_loss += loss.data[0]
+        epoch_accuracy   += accuracy
 
         # Save the trained model
         if (i + 1) == 1 and epoch % snapshot_epoch_interval == 0:
             exp.save_checkpoint(exp_name, epoch, net.state_dict(), optimizer.state_dict())
 
         iter_end = time.time()
-        # Display Training Progress
+
+        # Log Training Progress
         if (i + 1) % log_iter_interval == 0:
-            print('Epoch [%d/%d], Iter [%d/%d] Loss: %.2f Time Spent: %.2f sec'
-                  % (epoch, num_epochs, i + 1, len(data_loader), loss.data[0], iter_end - iter_start))
+            print('Epoch [%d/%d] Iter [%d/%d] Loss: %.2f Accuracy: %.4f Time Spent: %.2f sec'
+                  % (epoch, num_epochs, i + 1, len(data_loader), loss.data[0], accuracy, iter_end - iter_start))
 
         if DEBUG:
             print('Epoch {}, Iter {}, Loss {}'.format(epoch, i, loss.data[0]))
 
-    epoch_loss /= len(data_loader)
+    epoch_train_loss /= len(data_loader)
+    epoch_accuracy   /= len(data_loader)
+
+    if use_tensorboard:
+        experiment.add_scalar_value('train loss', epoch_train_loss, step=epoch)
+        # experiment.add_scalar_value('val loss', epoch_val_loss, step=epoch)
+        experiment.add_scalar_value('accuracy', epoch_accuracy, step=epoch)
+        # experiment.add_scalar_value('learning_rate', lr, step=epoch)
+
     epoch_end = time.time()
 
     print('Epoch [%d/%d], Loss: %.2f Time Spent: %.2f sec'
-          % (epoch, num_epochs, epoch_loss, epoch_end - epoch_start))
+          % (epoch, num_epochs, epoch_train_loss, epoch_end - epoch_start))
