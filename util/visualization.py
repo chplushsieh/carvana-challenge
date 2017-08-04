e30349e6b28a1c1ffacbbee0e5a7da726daf0257
judichunt <uioopoiuy@gmail.com>
2017-08-04 14:03:23 -0700

    Update visualization.py

--- util/visualization.py
+++ util/visualization.py
@@ -1,4 +1,6 @@
-import matplotlib
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
 
 def visualize(image, target):
     '''
@@ -6,10 +8,23 @@ def visualize(image, target):
       image:  a numpy array of shape (num of channels, Height, Width)
       target: a numpy array of shape (1, Height, Width) with only 1's and 0's in it
     '''
-    assert image.shape == target.shape
+       
+    assert image.shape[:-1] == target.shape
+    
 
-    # TODO use matplotlib to draw three figures side-by-side:
+
+    
+    
+    plt.imshow(target, 'gray', interpolation='none')
+    plt.imshow(image, 'gray', interpolation='none', alpha=0.6)
+    plt.show()  
+
+
+#example
+#input_path='/home/judichunt/Downloads/'
+#target = np.array(Image.open(input_path+'train_masks/00087a6bd4dc_01_mask.gif'), dtype=np.uint8)
+#image = np.array(Image.open(input_path+'train/00087a6bd4dc_01.jpg'), dtype=np.uint8)
+#visualize(image, target)
+
     # image, target, image overlaied with target
     # like so: https://kaggle2.blob.core.windows.net/forum-message-attachments/208218/6916/starter-kit.png
-
-    return
