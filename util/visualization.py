49dab107fd3cfac3e64fcec19b5000f05738a616
chplushsieh <chplushsieh@gmail.com>
2017-08-06 22:28:58 -0700

    converted from float numpy array to uint

--- util/visualization.py
+++ util/visualization.py
@@ -9,40 +9,42 @@ def visualize(image, pred, target=None):
       pred: a numpy array of shape (1, Height, Width) with numbers between 0 and 1 in it
       target: a numpy array of shape (1, Height, Width) with only 1's and 0's in it
     '''
+    image = image.astype('uint8')
+
     def change_index_ord(img):
         img=np.swapaxes(img,0,2)
         img=np.swapaxes(img,0,1)
         return img
     image=change_index_ord(image)
-    pred=np.squeeze(change_index_ord(pred) , axis=2)  
+    pred=np.squeeze(change_index_ord(pred) , axis=2)
     #print(image.shape)
     #print(pred.shape)
-    
-    
+
+
     assert image.shape[:-1] == pred.shape
-    
+
     plt.figure()
     plt.subplot(1,2,1)
     plt.imshow(image, 'gray', interpolation='none')
     plt.imshow(pred, 'BuGn', interpolation='none', alpha=0.3)
-    
-    
+
+
     plt.subplot(1,2,2)
     plt.imshow(pred, 'BuGn', interpolation='none')
     plt.show()
-    
+
     if target is not None:
-        target=np.squeeze(change_index_ord(target) , axis=2)  
+        target=np.squeeze(change_index_ord(target) , axis=2)
         plt.figure()
         plt.subplot(1,3,1)
         plt.imshow(image, 'gray', interpolation='none')
         plt.imshow(pred, 'BuGn', interpolation='none', alpha=0.4)
         plt.imshow(target, 'Purples', interpolation='none', alpha=0.3)
-        
-        
+
+
         plt.subplot(1,3,2)
         plt.imshow(pred, 'BuGn', interpolation='none')
-        
+
         plt.subplot(1,3,3)
         plt.imshow(target, 'Purples', interpolation='none')
         plt.show()
