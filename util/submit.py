b16221e3fef2ba3d0f7789d0d0c368a8149c9624
Unknown <uioopoiuy@gmail.com>
2017-08-11 00:48:44 -0700

    write a dictionary in csv

--- util/submit.py
+++ util/submit.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import pandas as pd
 
 import util.run_length as run_length
 import util.const as const
@@ -14,5 +15,8 @@ def save_predictions(exp_name, preds):
     save_path = os.path.join(const.OUTPUT_DIR, exp_name, 'submission.csv')
 
     # TODO iterate thru preds, use run_length.run_length_encode(mask) and save into a csv file
+    pd.DataFrame(list(preds.items()), columns=['image_name', 'mask'])
+    preds['mask']=preds['mask'].apply(lambda x: run_length.run_length_encode(x))
+    preds.tocsv(save_path)
 
     return
