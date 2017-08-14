8d6b096d9f7868e60a9fa851a60adadb4b67b505
Unknown <uioopoiuy@gmail.com>
2017-08-13 17:04:46 -0700

    add mask target boolean

--- util/visualization.py
+++ util/visualization.py
@@ -36,26 +36,29 @@ def visualize(image, pred, target=None):
     if target is not None:
         target=np.squeeze(change_index_ord(target) , axis=2)
         plt.figure()
-        plt.subplot(1,3,1)
+        plt.subplot(2,2,1)
         plt.imshow(image, 'gray', interpolation='none')
         plt.imshow(pred, 'BuGn', interpolation='none', alpha=0.4)
         plt.imshow(target, 'Purples', interpolation='none', alpha=0.3)
 
 
-        plt.subplot(1,3,2)
+        plt.subplot(2,2,2)
         plt.imshow(pred, 'BuGn', interpolation='none')
 
-        plt.subplot(1,3,3)
+        plt.subplot(2,2,3)
         plt.imshow(target, 'Purples', interpolation='none')
+
+        plt.subplot(2, 2, 4)
+        plt.imshow((pred+target*0.5), 'Accent', interpolation='none')
         plt.show()
         return
 
-#example
-#input_path='/home/judichunt/Downloads/'
-#target = np.array(Image.open(input_path+'train_masks/00087a6bd4dc_01_mask.gif'), dtype=np.uint8)
-#image = np.array(Image.open(input_path+'train/00087a6bd4dc_01.jpg'), dtype=np.uint8)
-#pred = np.array(Image.open(input_path+'train_masks/0cdf5b5d0ce1_01_mask.gif'), dtype=np.uint8)
-#visualize(image, pred, target)
+# example
+# input_path='/home/judichunt/Downloads/'
+# target = np.array(Image.open(input_path+'train_masks/00087a6bd4dc_01_mask.gif'), dtype=np.uint8)
+# image = np.array(Image.open(input_path+'train/00087a6bd4dc_01.jpg'), dtype=np.uint8)
+# pred = np.array(Image.open(input_path+'train_masks/0cdf5b5d0ce1_01_mask.gif'), dtype=np.uint8)
+# visualize(image, pred, target)
 
     # image, target, image overlaied with target
     # like so: https://kaggle2.blob.core.windows.net/forum-message-attachments/208218/6916/starter-kit.png
