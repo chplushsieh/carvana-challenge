5aa9346b3c849573e606697add0570f0e881089e
Unknown <uioopoiuy@gmail.com>
2017-08-16 07:09:10 -0700

    complete color transform with 0.5 probability

--- util/color.py
+++ util/color.py
@@ -1,3 +1,9 @@
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import cv2
+
+
 
 def transform(image):
     '''
@@ -8,6 +14,23 @@ def transform(image):
     '''
     transformed = image
 
-    # TODO
+    hue_shift_limit = (-50, 50)
+    sat_shift_limit = (-5, 5)
+    val_shift_limit = (-15, 15)
+
+    if np.random.random() < 0.5:
+        transformed = cv2.cvtColor(transformed, cv2.COLOR_BGR2HSV)
+        h, s, v = cv2.split(transformed)
+        hue_shift = np.random.uniform(hue_shift_limit[0], hue_shift_limit[1])
+        h = cv2.add(h, hue_shift)
+        sat_shift = np.random.uniform(sat_shift_limit[0], sat_shift_limit[1])
+        s = cv2.add(s, sat_shift)
+        val_shift = np.random.uniform(val_shift_limit[0], val_shift_limit[1])
+        v = cv2.add(v, val_shift)
+        transformed = cv2.merge((h, s, v))
+        transformed = cv2.cvtColor(transformed, cv2.COLOR_HSV2BGR)
+
+
 
     return transformed
+
