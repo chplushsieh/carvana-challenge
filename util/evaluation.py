6e00ac3dac01b6e66e3dc172ccb83e97ba69e6d1
Unknown <chplushsieh@gmail.com>
2017-08-20 15:29:40 -0700

    compete new dice loss

--- util/evaluation.py
+++ util/evaluation.py
@@ -9,14 +9,24 @@ https://www.kaggle.com/c/carvana-image-masking-challenge#evaluation
 
 #https://github.com/pytorch/pytorch/issues/1249
 def dice_loss(m1, m2 ):
+    '''
+    input:
+      m1: a pytorch Variable of size  (batch_size, num_channels, heiht, width)
+      m2:
+    output:
+      score: float
+    '''
+    m1 = m1.contiguous()
+    m2 = m2.contiguous()
+
     num = m1.size(0)
     m1  = m1.view(num,-1)
     m2  = m2.view(num,-1)
     intersection = (m1 * m2)
 
     score = 2. * (intersection.sum(1)+1) / (m1.sum(1) + m2.sum(1)+1)
-    score = score.sum()/num
-    return score
+    score = score.sum()/num  # a Variable of FloatTensor of size 1
+    return score.data[0]
 
 # def batch_dice(x, y):
 #     '''
