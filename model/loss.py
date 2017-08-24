9cc8205dbda59e4060028efde61cc95b2fe0f4db
chplushsieh <chplushsieh@gmail.com>
2017-08-23 23:38:47 -0700

    prepare for debugging weighted loss

--- model/loss.py
+++ model/loss.py
@@ -53,22 +53,22 @@ class BCELoss2d(nn.Module):
         targets_flat = targets.view(-1)
         return self.bce_loss(inputs_flat, targets_flat)
 
-# class WeightedBCELoss2d(nn.Module):
-#     '''
-#     from
-#     https://kaggle2.blob.core.windows.net/forum-message-attachments/212369/7045/weighted_dice_2.png
-#     '''
-#     def __init__(self):
-#         super(WeightedBCELoss2d, self).__init__()
-#
-#     def forward(self, inputs, targets, weights):
-#         inputs   = inputs.view (-1)
-#         targets = targets.view(-1)
-#         weights   = weights.view (-1)
-#
-#         loss = weights * inputs.clamp(min=0) - weights * inputs * targets + weights * torch.log(1 + torch.exp(-inputs.abs()))
-#         loss = loss.sum() / w.sum()
-#         return loss
+class WeightedBCELoss2d(nn.Module):
+    '''
+    from
+    https://kaggle2.blob.core.windows.net/forum-message-attachments/212369/7045/weighted_dice_2.png
+    '''
+    def __init__(self):
+        super(WeightedBCELoss2d, self).__init__()
+
+    def forward(self, inputs, targets, weights):
+        inputs   = inputs.view (-1)
+        targets = targets.view(-1)
+        weights   = weights.view (-1)
+
+        loss = weights * inputs.clamp(min=0) - weights * inputs * targets + weights * torch.log(1 + torch.exp(-inputs.abs()))
+        loss = loss.sum() / w.sum()
+        return loss
 
 
 class SoftDiceLoss(nn.Module):
@@ -89,26 +89,26 @@ class SoftDiceLoss(nn.Module):
         score = 1 - score.sum()/num
         return score
 
-# class WeightedSoftDiceLoss(nn.Module):
-#     '''
-#     from
-#     https://kaggle2.blob.core.windows.net/forum-message-attachments/212369/7045/weighted_dice_2.png
-#     '''
-#     def __init__(self):
-#         super(WeightedSoftDiceLoss, self).__init__()
-#
-#
-#     def forward(self, inputs, targets):
-#         num = targets.size(0)
-#         m1  = inputs.view(num,-1)
-#         m2  = targets.view(num,-1)
-#         w   = weights.view(num,-1)
-#         w2  = w * w
-#         intersection = (m1 * m2)
-#
-#         score = 2. * ((w2 * intersection).sum(1)+1) / ((w2 * m1).sum(1) + (w2 * m2).sum(1)+1)
-#         score = 1 - score.sum()/num
-#         return score
+class WeightedSoftDiceLoss(nn.Module):
+    '''
+    from
+    https://kaggle2.blob.core.windows.net/forum-message-attachments/212369/7045/weighted_dice_2.png
+    '''
+    def __init__(self):
+        super(WeightedSoftDiceLoss, self).__init__()
+
+
+    def forward(self, inputs, targets):
+        num = targets.size(0)
+        m1  = inputs.view(num,-1)
+        m2  = targets.view(num,-1)
+        w   = weights.view(num,-1)
+        w2  = w * w
+        intersection = (m1 * m2)
+
+        score = 2. * ((w2 * intersection).sum(1)+1) / ((w2 * m1).sum(1) + (w2 * m2).sum(1)+1)
+        score = 1 - score.sum()/num
+        return score
 
 class HengLoss(nn.Module):
     def __init__(self):
@@ -128,22 +128,22 @@ class StableHengLoss(nn.Module):
     def forward(self, inputs, targets):
         return self.dice(inputs, targets) + self.stable_bce(inputs, targets)
 
-# class BoundaryWeightedLoss(nn.Module):
-#     def __init__(self):
-#         super(BoundaryWeightedLoss, self).__init__()
-#         self.weighted_bce = WeightedBCELoss2d()
-#         self.weighted_dice = WeightedSoftDiceLoss()
-#
-#     def forward(self, inputs, targets):
-#         avg_neighbors = F.avg_pool2d(targets, kernel_size=11, padding=5, stride=1)
-#         is_boundary = avg_neighbors.ge(0.01) * avg_neighbors.le(0.99)
-#         is_boundary = is_boundary.float()
-#
-#         weights = Variable(torch.tensor.torch.ones(avg_neighbors.size())).cuda()
-#
-#         w0 = wieghts.sum()
-#         weights = weights + is_boundary * 2
-#         w1 = weights.sum()
-#         weights = weights * w0 / w1
-#
-#         return self.weighted_dice(inputs, targets, weights) + self.weighted_bce(inputs, targets, weights)
+class BoundaryWeightedLoss(nn.Module):
+    def __init__(self):
+        super(BoundaryWeightedLoss, self).__init__()
+        self.weighted_bce = WeightedBCELoss2d()
+        self.weighted_dice = WeightedSoftDiceLoss()
+
+    def forward(self, inputs, targets):
+        avg_neighbors = F.avg_pool2d(targets, kernel_size=11, padding=5, stride=1)
+        is_boundary = avg_neighbors.ge(0.01) * avg_neighbors.le(0.99)
+        is_boundary = is_boundary.float()
+
+        weights = Variable(torch.tensor.torch.ones(avg_neighbors.size())).cuda()
+
+        w0 = wieghts.sum()
+        weights = weights + is_boundary * 2
+        w1 = weights.sum()
+        weights = weights * w0 / w1
+
+        return self.weighted_dice(inputs, targets, weights) + self.weighted_bce(inputs, targets, weights)
