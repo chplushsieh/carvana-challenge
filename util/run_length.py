ca58cdd3390ce66bce26f718e37ab84ad4eefcde
Unknown <uioopoiuy@gmail.com>
2017-08-26 11:31:52 -0700

    rle debug

--- util/run_length.py
+++ util/run_length.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+from PIL import Image
 
 '''
 Carvana competition uses Run Length encoding to reduce the size of submission:
@@ -7,6 +8,19 @@ https://www.kaggle.com/c/carvana-image-masking-challenge#evaluation
 '''
 
 def encode(mask):
+    '''
+    img: numpy array, 1 - mask, 0 - background
+    Returns run length as string formated
+    '''
+    inds = mask.flatten()
+    runs = np.where(inds[1:] != inds[:-1])[0] + 2
+    runs[1::2] = runs[1::2] - runs[:-1:2]
+    rle = ' '.join([str(r) for r in runs])
+    return rle
+
+
+
+def encode_old(mask):
     '''
     input:
       mask: a numpy array with only 0's or 1's in it
@@ -49,10 +63,13 @@ def encode(mask):
 if __name__ == "__main__":
 
     test_arr = np.array([
-        [1, 0, 0, 1, 1],
+        [0, 0, 0, 1, 1],
         [0, 1, 0, 1, 0],
         [1, 1, 0, 1, 0]
     ])
+    test_arr = np.array(Image.open('/home/judichunt/git/carvana-challenge/data/train_masks/0d53224da2b7_04_mask.gif'),
+               dtype=np.uint8)
+    print(encode(test_arr))
 
-    stringified_output = '1 1 3 1 5 2 10 4'
-    assert stringified_output == encode(test_arr)
+    # stringified_output = '1 1 3 1 5 2 10 4'
+    # assert stringified_output == encode(test_arr)
