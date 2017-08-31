11478ecaa0d288d9fe76a859693adee34d53c286
Unknown <chplushsieh@gmail.com>
2017-08-31 15:29:08 -0700

    switched to newly-released high quality dataset

--- util/const.py
+++ util/const.py
@@ -2,9 +2,9 @@ import os.path
 
 DATA_DIR = './data'
 
-TRAIN_DIR = os.path.join(DATA_DIR, 'train')
+TRAIN_DIR = os.path.join(DATA_DIR, 'train_hq')
 TRAIN_MASK_DIR = os.path.join(DATA_DIR, 'train_masks')
-TEST_DIR = os.path.join(DATA_DIR, 'test')
+TEST_DIR = os.path.join(DATA_DIR, 'test_hq')
 
 TRAIN_IMAGESET_PATH = os.path.join(DATA_DIR, 'train.csv')
 VAL_IMAGESET_PATH   = os.path.join(DATA_DIR, 'val.csv')
