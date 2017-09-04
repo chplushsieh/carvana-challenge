b72207d276cabeea41e58937bf0c8d8faba6b3a3
chplushsieh <chplushsieh@gmail.com>
2017-09-03 19:58:45 -0700

    apply_CRF function draft done

--- util/crf.py
+++ util/crf.py
@@ -5,10 +5,11 @@ import pydensecrf.densecrf as dcrf
 from pydensecrf.utils import compute_unary, create_pairwise_bilateral, create_pairwise_gaussian, softmax_to_unary
 
 
-def apply_crf(masks, probs):
+def apply_crf(img, prob):
     '''
     input:
-      masks: pytorch Variable in gpu
+      img: numpy array of shape (num of channels, height, width)
+      prob: numpy array of shape (height, width), neural network last layer sigmoid output for img
 
     Modified from:
     https://github.com/yt605155624/tensorflow-deeplab-resnet/blob/e81482d7bb1ae674f07eae32b0953fe09ff1c9d1/inference_crf.py
@@ -17,24 +18,25 @@ def apply_crf(masks, probs):
       masks = crf.apply_crf(masks, outputs)
     '''
 
-    unary = softmax_to_unary(probs)
-    unary = np.ascontiguousarray(unary)
+    # preprocess prob to (num_classes, height, width) since we have 2 classes: car and background.
+    num_classes = 2
+    probs = np.zeros((num_classes, prob.shape[0], prob.shape[1]))
+    probs[0] = np.subtract(1, prob) # class 0 is background
+    probs[1] = prob                 # class 1 is car
 
-    # TODO
-    num_classes = 1 # or 2?
-    # img.shape = ?
+    d = dcrf.DenseCRF(img.shape[1], img.shape[2], num_classes)
 
-    d = dcrf.DenseCRF(img.shape[0] * img.shape[1], num_classes)
+    unary = unary_from_softmax(prob)
+    # unary = np.ascontiguousarray(unary)
     d.setUnaryEnergy(unary)
-    # TODO unary protential is not dependent on the image itself?
 
     # This potential penalizes small pieces of segmentation that are
     # spatially isolated -- enforces more spatially consistent segmentations
-    feats = create_pairwise_gaussian(sdims=(10, 10), shape=img.shape[:2])
+    feats = create_pairwise_gaussian(sdims=(10, 10), shape=img.shape[1:])
     d.addPairwiseEnergy(feats, compat=3,
                         kernel=dcrf.DIAG_KERNEL,
                         normalization=dcrf.NORMALIZE_SYMMETRIC)
-    # TODO this potential is not dependent on the image itself?
+    # Note that this potential is not dependent on the image itself.
 
     # This creates the color-dependent features --
     # because the segmentation that we get from CNN are too coarse
@@ -48,6 +50,6 @@ def apply_crf(masks, probs):
 
 
     Q = d.inference(5)  # set the number of iterations
-    res = np.argmax(Q, axis=0).reshape((img.shape[0], img.shape[1]))
+    res = np.argmax(Q, axis=0).reshape((img.shape[1], img.shape[2]))
 
     return res
