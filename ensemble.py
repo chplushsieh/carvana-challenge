de918fd6bcc7a95409f9887fa6f83568154b48ce
chplushsieh <chplushsieh@gmail.com>
2017-09-12 20:57:05 -0700

    removed comments

--- ensemble.py
+++ ensemble.py
@@ -8,12 +8,6 @@ import util.evaluation as evaluation
 import util.visualization as viz
 import util.submit as submit
 
-# TODO
-# load predictions from multiple output/<exp_name>/predictions folders
-# take average of them
-# convert from prob to mask
-# apply RLE
-# save into submission.csv
 
 def ensemble(ensembler_loader):
     img_rles = {}
