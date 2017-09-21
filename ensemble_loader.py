9c28773e051f039837683b95920590c4d02a4caf
chplushsieh <chplushsieh@gmail.com>
2017-09-21 06:32:10 -0700

    create models_ensembled.txt for ensemble

--- ensemble_loader.py
+++ ensemble_loader.py
@@ -24,7 +24,12 @@ class EnsembleRunner(torch.utils.data.dataset.Dataset):
 
         self.ensemble_dir = get_time.get_current_time()
 
-        # TODO update ensemble_dir/models.txt
+        # create self.ensemble_dir/models_ensembled.txt
+        for pred_dir in self.pred_dirs:
+            exp_names, test_time_aug_names = get_models_ensembled(pred_dir)
+
+            for exp_name, test_time_aug_name in zip(exp_names, test_time_aug_names):
+                ensemble.mark_model_ensembled(self.ensemble_dir, exp_name, test_time_aug_name)
         return
 
     def __len__(self):
